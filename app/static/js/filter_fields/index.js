b425bd41852292c883de175b6ed4e0c8963a9c0e
niux3 <renaudbourdeau@gmail.com>
2025-01-28 17:33:08 +0100

    feat: remove index.js (filter fields)

--- app/static/js/filter_fields/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FilterFieldsModel } from './FilterFieldsModel.js'
-export { default as FilterFieldsView } from './FilterFieldsView.js'
-export { default as FilterFieldsController } from './FilterFieldsController.js'
